c9a069d188d33a61442157d143d5adaedf3aedbd
FELIPE MARTINS SANCHES <a2390809@debian.dacom>
2022-04-07 15:32:12 -0300

    aula0704 p2

--- aula31/03/aula0704/main.c
+++ aula31/03/aula0704/main.c
@@ -20,13 +20,34 @@ bool twosum_bruteforce(int*v, int n, int x){
         return false;
 }
 
+bool twosum_sort(int *v, int n, int x){
+    int i= 0, j = n-1;
+    quicksort(v, n);
+    while(i!=j){
+        int soma = v[i] + v[j];
+        if (soma == x){
+        printf("%d + %d = %d\n", v[i], v[j], x);
+        return true;
+        }
+        else{
+            if (x > soma){
+                i++;
+            }
+            if (x<soma){
+                j--;
+            }
+        }
+    }
+    return false;
+}
+
 int main(int argc, char** argv){
     //int v[]= {2, -1, 5, 8, 7, 4};
     //int n = 12;
     int n = atoi(argv[1]);
     int *v = random_vector(n, n, 999);
-    quicksort(v, n);
-    twosum_bruteforce(v, n, 80);
+
+    twosum_sort(v, n, 10);
 
     return EXIT_SUCCESS;
 }
